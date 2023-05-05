0db4a93c915d4575983f174e15c203e9fddb91a7
EstelaRenderos <erenderos@e-coffeetech.com>
2023-05-05 07:39:01 -0700

    Primer commit : Se realizo la conexion a la DB, se creo la pagina principal, y los controladores y Views iniciales de la App.

---
+++ Planillas/Controllers/AREAController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planillas.Controllers
+{
+    public class AREAController : Controller
+    {
+        // GET: AREA
+        public ActionResult AREA()
+        {
+            return View();
+        }
+
+        
+        // GET: AREA/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AREA/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AREA/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AREA/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AREA/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AREA/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AREA/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Planillas/Controllers/EMPLEADOController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planillas.Controllers
+{
+    public class EMPLEADOController : Controller
+    {
+        // GET: EMPLEADO
+        public ActionResult EMPLEADO()
+        {
+            return View();
+        }
+
+        // GET: EMPLEADO/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EMPLEADO/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EMPLEADO/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EMPLEADO/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EMPLEADO/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EMPLEADO/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EMPLEADO/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
