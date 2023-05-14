86d20105ac0d84d8516e42edd313339893b9c5d1
EstelaRenderos <studt.estela@gmail.com>
2023-05-13 22:47:12 -0700

    Se agrego un controlador global para utilizar un enum que permite hacer el crud de cada controller, junto con la clase de de la DB, se termino de agregar los cambios de diseño y crud de MTTO para áreas utilizando un procemidimiento almacenado asignado a esta tarea.

--- Planillas/Controllers/AREAController.cs
+++ Planillas/Controllers/AREAController.cs
@@ -6,36 +6,30 @@ using System.Web.Mvc;
 
 namespace Planillas.Controllers
 {
-    public class AREAController : Controller
+    public class AREAController : ControlsController
     {
-        // GET: AREA
+       
+
         public ActionResult AREA()
         {
-            return View();
+            List<AREA> Modelo = (from c in db.AREA select c).ToList();
+
+            return View(Modelo);
         }
 
-        
-        // GET: AREA/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: AREA/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: AREA/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(FormCollection collection, AREA modelo)
         {
             try
             {
-                // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
+                db.SP_MTTO_AREAS(modelo.CODIGO_AREA, modelo.NOMBRE_AREA, (int)opcion.Add);
+                return RedirectToAction("AREA");
             }
             catch
             {
@@ -43,43 +37,50 @@ namespace Planillas.Controllers
             }
         }
 
-        // GET: AREA/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var Modelo = (from c in db.AREA where c.CODIGO_AREA == id select c).Single();
+
+            return View(Modelo);
         }
 
-        // POST: AREA/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(FormCollection collection, AREA modelo)
         {
             try
             {
-                // TODO: Add update logic here
+                db.SP_MTTO_AREAS(modelo.CODIGO_AREA, modelo.NOMBRE_AREA, (int)opcion.Update);
 
-                return RedirectToAction("Index");
+                return RedirectToAction("AREA");
             }
             catch
             {
                 return View();
             }
         }
-
-        // GET: AREA/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Details(int id)
         {
-            return View();
+            var Modelo = (from c in db.AREA where c.CODIGO_AREA == id select c).Single();
+
+            return View(Modelo);
         }
 
-        // POST: AREA/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            var Modelo = (from c in db.AREA where c.CODIGO_AREA == id select c).Single();
+
+            return View(Modelo);
+        }
+
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
             try
             {
-                // TODO: Add delete logic here
+                db.SP_MTTO_AREAS(id, "", (int)opcion.Delete);
 
-                return RedirectToAction("Index");
+                return View();
             }
             catch
             {
---
+++ Planillas/Controllers/ControlsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Planillas.Controllers
+{
+    public class ControlsController : Controller
+    {
+        public DBPlanillasDataContext db = new DBPlanillasDataContext();
+        public enum opcion
+        {
+            Add = 1,
+            Update = 2,
+            Delete = 3
+        }
+    }
+}
